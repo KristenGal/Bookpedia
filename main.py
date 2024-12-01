801f83cbc265cbbb462db937d92d6fb5f59fab35
Kristen Gal <elektrokristi@gmail.com>
2024-12-01 14:11:56 +0200

    add put, patch and delete

--- main.py
+++ main.py
@@ -22,7 +22,8 @@ def add_book():
     data = request.get_json()
 
     try:
-        book = Book(title=data.get("title"), 
+        book = Book(id=data.get("id"), 
+                    title=data.get("title"), 
                     author=data.get("author"), 
                     year=data.get("year"),
                     description=data.get("description"),
@@ -36,17 +37,53 @@ def add_book():
         return jsonify({"error": "Enter all values. Title, author, description and image_url (optional) must be string. Year must be integer. Rating (optional) must be float or integer"})
 
 
-@app.put("/")
+@app.put("/put")
 def put():
-    pass
+    data = request.get_json()
+    try:
+        book = Config.SESSION.get(Book, data.get("id"))
+        if not book or type(data.get("id")) != int:
+            return jsonify({"error": "Enter the valid id (integer)"})
+        Config.SESSION.delete(book)
 
-@app.patch("/")
+        newbook = Book(id=data.get("id"),
+                  title=data.get("title"), 
+                  author=data.get("author"), 
+                  year=data.get("year"),
+                  description=data.get("description"),
+                  rating=data.get("rating", 0),
+                  image_url=data.get("image_url", "https://picsum.photos/200/300?blur=5"))
+        Config.SESSION.add(newbook)
+        Config.SESSION.commit()
+        return jsonify({"success": "Successful operation"})
+    except:
+        return jsonify({"error": "Enter all values. Title, author, description and image_url (optional) must be string. Year must be integer. Rating (optional) must be float or integer"})
+
+
+@app.patch("/patch")
 def patch():
-    pass
+    data = request.get_json()
+    try:
+        book = Config.SESSION.get(Book, data.get("id"))
+        if not book:
+            return jsonify({"error": "Enter the valid id (integer)"})
+        book.sqlmodel_update(data)
+        Config.SESSION.commit()
+        return jsonify({"success": "Successful operation"})
+    except:
+        return jsonify({"error": "Title, author, description and image_url must be string. Year must be integer. Rating must be float or integer"})
 
-@app.delete("/")
-def delete():
-    pass
+
+@app.delete("/delete")
+def delete_book():
+    data = request.get_json()
+    try:
+        book = Config.SESSION.get(Book, data.get("id"))
+        Config.SESSION.delete(book)
+        Config.SESSION.commit()
+        return jsonify({"success": "Successful operation"})
+    except:
+        return jsonify({"error": "Enter the valid id (integer)"})
 
 
 if __name__ == "__main__":
